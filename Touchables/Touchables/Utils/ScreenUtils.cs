0411e12f2706eb6e44265c933fdfda1c9d7cc94f
francescoStrada <francesco.strada11@gmail.com>
2015-11-13 18:55:43 +0100

    Initial Commit
    First commit

---
+++ Touchables/Touchables/Utils/ScreenUtils.cs
@@ -0,0 +1,34 @@
+﻿/*
+ * @author Francesco Strada 
+ */
+
+using UnityEngine;
+
+namespace Touchables.Utils
+{
+    /// <summary>
+    /// Utils functions for touch screen related operations
+    /// </summary>
+    internal static class ScreenUtils
+    {
+        /// <summary>
+        /// Converts pixels value to centimeters according to screen dpi through Unity's Screen. <seealso cref="Screen"/>
+        /// </summary>
+        /// <param name="pxValue">Value in pixels</param>
+        /// <returns>Equivalent value in centimeters</returns>
+        public static float PixelsToCm(float pxValue)
+        {
+            return (pxValue / Screen.dpi) * 2.54f;
+        }
+
+        /// <summary>
+        /// Converts centimeters value to pixels according to screen dpi through Unity's Screen.dpi. <seealso cref="Screen"/>
+        /// </summary>
+        /// <param name="cmValue">Value in centimeters</param>
+        /// <returns>Equivalente value in pixels</returns>
+        public static float CmToPixels(float cmValue)
+        {
+            return ((cmValue * Screen.dpi) / 2.54f);
+        }
+    }
+}
